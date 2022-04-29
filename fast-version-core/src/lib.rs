b5a164c843c3f2b97f4e9903c0256f3ce7eb7203
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-04-29 18:47:11 +0200

    Added additional line in documentation

--- fast-version-core/src/lib.rs
+++ fast-version-core/src/lib.rs
@@ -1,4 +1,6 @@
 //! Core definitions for the fast-version crate
+//!
+//! Refer to the [fast-version](https://crates.io/crates/fast-version) for usage and documentation.
 
 pub mod version;
 pub mod version_req;
